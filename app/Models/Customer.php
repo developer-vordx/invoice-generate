0f5e3929f16730c47f422791ea33e3ab7be6cb40
hamza gill <hamzamux10@gmail.com>
2025-10-03 02:13:28 +0500

    init

---
+++ app/Models/Customer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'stripe_customer_id',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'address',
+        'postal_code',
+        'city',
+        'state',
+        'country',
+    ];
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
+}
